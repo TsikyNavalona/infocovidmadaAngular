74044ea7d68aea6cf4b0a4519bda34cbad7baaff
TsikyNavalona <tsikywatib@gmail.com>
2021-04-30 10:42:10 +0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true
 };
-export const base_url = 'http://tsiky.alwaysdata.net/InfoCovidMada/api/';
+export const base_url = 'https://tsiky.alwaysdata.net/InfoCovidMada/api/';
